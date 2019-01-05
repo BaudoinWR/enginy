5b851fd1ef6223b2baf72b9e1a0933eec51244ea
Baudoin Woods Ribadeau <baudoin.wr@gmail.com>
2019-01-05 23:15:01 +0100

    Text UI

---
+++ src/main/java/com/woobadeau/tinyengine/things/ui/Text.java
@@ -0,0 +1,27 @@
+package com.woobadeau.tinyengine.things.ui;
+
+import com.woobadeau.tinyengine.things.Thing;
+import com.woobadeau.tinyengine.things.physics.Vector2D;
+
+import java.awt.*;
+
+public class Text extends Thing {
+
+    protected String string;
+    private Font font;
+    private Color color;
+
+    public Text(String string, Font font, Color color, Vector2D position) {
+        this.string = string;
+        this.font = font;
+        this.color = color;
+        this.move(position);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setFont(font);
+        graphics.setColor(color);
+        graphics.drawString(string, getPosition().x, getPosition().y);
+    }
+}
