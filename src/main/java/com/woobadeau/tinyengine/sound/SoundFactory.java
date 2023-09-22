7b06315ab66572044fc039d174a1ceb77c052dda
Baudoin Woods Ribadeau <baudoin.wr@gmail.com>
2023-09-22 15:03:18 +0100

    Fix mark/reset issue

--- src/main/java/com/woobadeau/tinyengine/sound/SoundFactory.java
+++ src/main/java/com/woobadeau/tinyengine/sound/SoundFactory.java
@@ -1,6 +1,8 @@
 package com.woobadeau.tinyengine.sound;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -9,7 +11,8 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class SoundFactory {
     public static Clip getClip(String resourceName) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(SoundFactory.class.getResourceAsStream(resourceName));
+        InputStream resourceAsStream = SoundFactory.class.getResourceAsStream(resourceName);
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(resourceAsStream));
         Clip clip = AudioSystem.getClip();
         clip.open(audioInputStream);
         return clip;
